09886a283fffcab56aa7adda4405c7ded3cc491d
Do-Thomas <94061934+Do-Thomas@users.noreply.github.com>
2023-04-01 20:35:13 +0700

    Add files via upload

---
+++ ThiThu_SU22/src/Q1/Boo.java
@@ -0,0 +1,21 @@
+package Q1;
+
+// =========== DO NOT EDIT THE GIVEN CONTENT OF THIS FILE ============
+class Boo {
+
+    String forest;
+    int rate, sound;
+
+    Boo() {
+    }
+
+    Boo(String xForest, int xRate, int xSound) {
+        forest = xForest;
+        rate = xRate;
+        sound = xSound;
+    }
+
+    public String toString() {
+        return ("(" + forest + "," + rate + "," + sound + ")");
+    }
+}
---
+++ ThiThu_SU22/src/Q1/Node.java
@@ -0,0 +1,20 @@
+package Q1;
+
+// =========== DO NOT EDIT THE GIVEN CONTENT OF THIS FILE ============
+class Node {
+
+    Boo info;
+    Node next;
+
+    Node() {
+    }
+
+    Node(Boo x, Node p) {
+        info = x;
+        next = p;
+    }
+
+    Node(Boo x) {
+        this(x, null);
+    }
+}
